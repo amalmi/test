b6133b6126419c5b4edbe6baab713ee6d930cab5
Antti Malmi <Antti.Malmi@paf.com>
2018-04-20 16:32:50 +0300

    refactoring.

--- js/Ground.js
+++ js/Ground.js
@@ -1,70 +1,66 @@
 class Ground {
     constructor(app) {
         this.app = app;
-        //
-        //
-        //
-        //
+        let numPoints =20;
+        let xWidth = 1280 * 2;
 
-        let numPoints = 100;
-        /*
-        var tilingSprite = new PIXI.extras.TilingSprite(
-            texture,
-            this.app.screen.width,
-            this.app.screen.height
+        this.texture = PIXI.Texture.fromImage('p2.jpeg');
+
+//        this.renderTexture = new PIXI.RenderTexture(this.texture);
+
+        this.renderTexture = PIXI.RenderTexture.create(
+            xWidth,
+            256
         );
 
-        this.app.stage.addChild(tilingSprite);
-        */
 
-        var count = 0;
+        this.tilingSprite = new PIXI.extras.TilingSprite(
+            this.texture,
+            xWidth,
+            256
+        );
 
-// build a rope!
-        var ropeLength = 550 / numPoints;
+//        this.app.stage.addChild(this.tilingSprite);
 
-        var points = [];
+        let count = 0;
 
-        for (var i = 0; i < numPoints + 1; i++) {
+        // build a rope!
+        let ropeLength = (xWidth)/ numPoints;
+        let points = [];
+        for (let i = 0; i < numPoints + 1; i++) {
             points.push(new PIXI.Point(i * ropeLength, 0));
         }
-
-
-        var strip = new PIXI.mesh.Rope(PIXI.Texture.fromImage('snake.png'), points);
-
-
-
-
-//strip.x = -459;
+        let strip = new PIXI.mesh.Rope(this.renderTexture, points);
+        //strip.x = -459;
 
         var snakeContainer = new PIXI.Container();
-        snakeContainer.x = 1280 / 2;
-        snakeContainer.y = 720 / 2 + 800;
-
-        snakeContainer.pivot.set(0.5, 0.5);
-
+//        snakeContainer.pivot.set(0.5, 0.5);
+        snakeContainer.x = 0;
+        snakeContainer.y = 720 * 2;
         snakeContainer.scale.set(1);
         this.app.stage.addChild(snakeContainer);
-
-
         snakeContainer.addChild(strip);
 
         this.app.ticker.add(function() {
+            count = Math.PI * 1.35;
+//            count +=0.01;
+            console.log(count);
 
-//    count += 0.01;
-            count = (Math.PI * 2) / numPoints / 6;
-            snakeContainer.rotation -=0.005;
+            this.app.renderer.render(this.tilingSprite, this.renderTexture, false);
+
+//            this.tilingSprite.tilePosition.x += 1;
+            this.tilingSprite.tilePosition.x -= 4;
+
             // make the snake
-            /*
             for (var i = 0; i < points.length; i++) {
-                points[i].y = Math.sin((i * 0.3) + count) * 250;
-                points[i].x = i * ropeLength + Math.cos((i * 0.1) + count) * 250;
+                points[i].y = Math.sin((i * 0.1) + count) * 1000 ;
+                points[i].x = i * ropeLength;
             }
-            */
-
+/*
             for(let i = 0; i < points.length; i++){
-                points[i].y = 800 * Math.cos(count * i);
-                points[i].x = 800 * Math.sin(count * i);
-            }
+                points[i].y = 256 * Math.cos(count * i);
+                points[i].x = 256 * Math.sin(count * i) * 4;
+            } */
         }.bind(this));
     }
 }
