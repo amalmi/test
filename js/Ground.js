ee086b921b13eeb8251923af026357afcfe75d5d
Antti Malmi <anttijmalmi@gmail.com>
2018-04-22 09:17:49 +0300

    more controls

--- js/Ground.js
+++ js/Ground.js
@@ -2,23 +2,27 @@ class Ground {
     constructor(app) {
         this.app = app;
         let numPoints =20;
-        let xWidth = 1280 * 2;
+        this.widthTexture = 1280 * 2;
+        this.heightTexture = 256;
+        this.curve = 1000;
+
         this.speed = 0;
+        this.scale = 1;
 
         this.texture = PIXI.Texture.fromImage('p2.jpeg');
 
 //        this.renderTexture = new PIXI.RenderTexture(this.texture);
 
         this.renderTexture = PIXI.RenderTexture.create(
-            xWidth,
-            256
+            this.widthTexture,
+            this.heightTexture
         );
 
 
         this.tilingSprite = new PIXI.extras.TilingSprite(
             this.texture,
-            xWidth,
-            256
+            this.widthTexture,
+            this.heightTexture
         );
 
 //        this.app.stage.addChild(this.tilingSprite);
@@ -26,26 +30,29 @@ class Ground {
         let count = 0;
 
         // build a rope!
-        let ropeLength = (xWidth)/ numPoints;
+        let ropeLength = (this.widthTexture)/ numPoints;
         let points = [];
         for (let i = 0; i < numPoints + 1; i++) {
             points.push(new PIXI.Point(i * ropeLength, 0));
         }
-        let strip = new PIXI.mesh.Rope(this.renderTexture, points);
+        this.strip = new PIXI.mesh.Rope(this.renderTexture, points);
+        this.strip.pivot.set(0.5, 0.5);
+        this.strip.x =  this.scale * this.widthTexture / 2 - this.widthTexture / 2;
+        this.strip.y = -this.scale * this.heightTexture - this.heightTexture / 2;
+
+
         //strip.x = -459;
 
-        var snakeContainer = new PIXI.Container();
-//        snakeContainer.pivot.set(0.5, 0.5);
-        snakeContainer.x = 0;
-        snakeContainer.y = 720 * 2;
-        snakeContainer.scale.set(1);
-        this.app.stage.addChild(snakeContainer);
-        snakeContainer.addChild(strip);
+        this.snakeContainer = new PIXI.Container();
+//        this.snakeContainer.pivot.set(0.5, 0.5);
+//        this.snakeContainer.x = -this.app.width / 2;
+        this.snakeContainer.y = this.app.renderer.height * 2;
+        this.app.stage.addChild(this.snakeContainer);
+        this.snakeContainer.addChild(this.strip);
 
         this.app.ticker.add(function() {
             count = Math.PI * 1.35;
 //            count +=0.01;
-            console.log(count);
 
             this.app.renderer.render(this.tilingSprite, this.renderTexture, false);
 
@@ -54,7 +61,7 @@ class Ground {
 
             // make the snake
             for (var i = 0; i < points.length; i++) {
-                points[i].y = Math.sin((i * 0.1) + count) * 1000 ;
+                points[i].y = Math.sin((i * 0.1) + count) * this.curve ;
                 points[i].x = i * ropeLength;
             }
 /*
@@ -64,7 +71,22 @@ class Ground {
             } */
         }.bind(this));
     }
-    onChange(value) {
-        this.speed = value
+    onChange({speed, scale, curve, x, y}) {
+        this.speed = speed ? speed : this.speed;
+        this.scale = scale ? scale : this.scale;
+        this.curve = curve ? curve : this.curve;
+        this.strip.x = x ? x : this.strip.x;
+        this.strip.y = y ? y : this.strip.y;
+
+        if (scale) {
+//            this.snakeContainer.scale.set(this.scale);
+            this.strip.scale.set(this.scale);
+//            this.snakeContainer.y = this.app.renderer.height / 2;
+//            this.snakeContainer.x = this.app.renderer.width * this.scale;
+
+//            this.strip.x =  this.scale * this.widthTexture / 2 - this.widthTexture / 2;
+//            this.strip.y = -this.scale * this.heightTexture - this.heightTexture / 2;
+
+        }
     }
 }
--- js/Main.js
+++ js/Main.js
@@ -9,23 +9,45 @@ class Main {
 
         let obj = {
             speed: 0.0,
-
-            color0: "#ffae23", // CSS string
-            color1: [ 0, 128, 255 ], // RGB array
-            color2: [ 0, 128, 255, 0.3 ], // RGB with alpha
-            color3: { h: 350, s: 0.9, v: 0.3 } // Hue, saturation, value
+            scale: 1,
+            curve: 1000,
+            x: 0,
+            y: 256
         };
 
+        ground.onChange(obj);
+
         let gui = new dat.gui.GUI();
+//        gui.remember(obj);
+        gui.add(obj, 'speed').min(-10).max(10).step(0.25)
+            .onChange(function(value) {
+                // Fires on every change, drag, keypress, etc.
+                ground.onChange({speed: value});
+            }.bind(this));
 
-        gui.remember(obj);
+        gui.add(obj, 'scale').min(0.5).max(2).step(0.025)
+            .onChange(function(value) {
+                // Fires on every change, drag, keypress, etc.
+                ground.onChange({scale: value});
+            }.bind(this));
 
-        let speedController = gui.add(obj, 'speed').min(-10).max(10).step(0.25);
+        gui.add(obj, 'curve').min(0).max(2000).step(1)
+            .onChange(function(value) {
+                // Fires on every change, drag, keypress, etc.
+                ground.onChange({curve: value});
+            }.bind(this));
 
-        speedController.onChange(function(value) {
-            // Fires on every change, drag, keypress, etc.
-            ground.onChange(value);
-        }.bind(this));
+        gui.add(obj, 'x').min(-2000).max(2000).step(1)
+            .onChange(function(value) {
+                // Fires on every change, drag, keypress, etc.
+                ground.onChange({x: value});
+            }.bind(this));
+
+        gui.add(obj, 'y').min(-2000).max(2000).step(1)
+            .onChange(function(value) {
+                // Fires on every change, drag, keypress, etc.
+                ground.onChange({y: value});
+            }.bind(this));
 
     }
 }
